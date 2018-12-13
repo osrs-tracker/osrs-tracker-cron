4aa895a6dbcb1120ea46f0d85b0a63139873eb18
Freek Mencke <info@toxsickproductions.com>
2018-12-13 14:00:55 +0100

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-REPOSITRY="cloud.canister.io:5000"
 NAME=$(node -p "require('./package.json').name");
 VERSION=$(node -p "require('./package.json').version");
 
 # secure canister login
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin $REPOSITRY
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
-docker build -t "$REPOSITRY/$DOCKER_USERNAME/$NAME:$VERSION" -t "$REPOSITRY/$DOCKER_USERNAME/$NAME:latest" .
-docker push "$REPOSITRY/$DOCKER_USERNAME/$NAME:$VERSION"
-docker push "$REPOSITRY/$DOCKER_USERNAME/$NAME:latest"
+docker build -t "$DOCKER_USERNAME/$NAME:$VERSION" -t "$DOCKER_USERNAME/$NAME:latest" .
+docker push "$DOCKER_USERNAME/$NAME:$VERSION"
+docker push "$DOCKER_USERNAME/$NAME:latest"
 
 curl -X POST $DOCKER_WEBHOOK
