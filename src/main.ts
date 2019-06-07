dd7faef0e394a531a7d36bbd767057a59870d1d7
Freek Mencke <info@toxsickproductions.com>
2019-06-07 17:48:51 +0200

    Added prometheus logging

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import cluster from 'cluster';
+import { collectDefaultMetrics } from 'prom-client';
 import { startHealthCheckServer } from './app/common/health-server';
 import { Logger } from './app/common/logger';
+import { startMetricsServer } from './app/common/metrics-server';
 import { Tasks } from './app/tasks';
 
 if (cluster.isMaster) {
   Logger.log('SERVER ACTIVE - FORKING WORKER');
   cluster.fork();
 
+  startMetricsServer();
+
   cluster.on('exit', (worker: cluster.Worker) => {
     Logger.log(`WORKER ${worker.id} DIED - CREATING NEW WORKER`);
     cluster.fork();
   });
 } else {
+  collectDefaultMetrics();
+
   Logger.log(`WORKER ${cluster.worker.id} CREATED - INITIALISING TASKS`);
   Tasks.start();
   startHealthCheckServer();
